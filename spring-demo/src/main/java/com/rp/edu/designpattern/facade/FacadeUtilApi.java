fd4ed03f671ea5a911b93a701adfaefd5e0109b5
rp215 <48685443+rp215@users.noreply.github.com>
2020-03-05 11:35:18 +0800

    Create FacadeUtilApi.java

---
+++ spring-demo/src/main/java/com/rp/edu/designpattern/facade/FacadeUtilApi.java
@@ -0,0 +1,12 @@
+package com.rp.edu.designpattern.facade;
+
+/**
+ * 门面模式，在spring中的三层架构就门面模式的体现，也包括以 util 结尾的类基本上都属于门面模式
+ * 对客户端提供一个统一的接口，而在内部去调用不同的服务
+ */
+public class FacadeUtilApi {
+
+    public void request(GiftPojo giftPojo) {
+
+    }
+}
